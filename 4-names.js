1302801f4c35a3409c005706d71a2e0098f131cc
Uttam Marandi <uttammarandi007@gmail.com>
2021-10-07 18:43:57 +0530

    module , exports, require

--- 4-names.js
+++ 4-names.js
@@ -1,6 +1,55 @@
 //Modules
+
+//local to the file
 const secret = "SUPER SECRET"; //this variable is availaible to me everywhere
+
+//share with other files
 const john = "john";
 const peter = "peter";
 
 console.log(module);
+module.exports = {
+  john: john,
+  peter: peter,
+};
+
+console.log(module);
+
+//logs
+// Module {
+//   id: '.',
+//   path: 'E:\\Acode\\Javascript\\Node\\Node Basics',
+//   exports: {},
+//   parent: null,
+//   filename: 'E:\\Acode\\Javascript\\Node\\Node Basics\\4-names.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     'E:\\Acode\\Javascript\\Node\\Node Basics\\node_modules',
+//     'E:\\Acode\\Javascript\\Node\\node_modules',
+//     'E:\\Acode\\Javascript\\node_modules',
+//     'E:\\Acode\\node_modules',
+//     'E:\\node_modules'
+//   ]
+// }
+
+//the exports key is the interseting one. whatever is present in the exports file can be accessed
+
+//after declaring module.exports the second module log logs
+// Module {
+//   id: '.',
+//   path: 'E:\\Acode\\Javascript\\Node\\Node Basics',
+//   exports: { john: 'john', peter: 'peter' },
+//   parent: null,
+//   filename: 'E:\\Acode\\Javascript\\Node\\Node Basics\\4-names.js',
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     'E:\\Acode\\Javascript\\Node\\Node Basics\\node_modules',
+//     'E:\\Acode\\Javascript\\Node\\node_modules',
+//     'E:\\Acode\\Javascript\\node_modules',
+//     'E:\\Acode\\node_modules',
+//     'E:\\node_modules'
+//   ]
+// }
+//we can see that john and peter are now values to exports to key. meaning they are sharable.
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 //Common JS : Every file in node is a module by default
 //Modules : Only share minimum, Encapsulated code
 
+const names = require("./4-names");
+//access anything that is exported from 4-names
+console.log(names);
+//we can access john and peter now
+const sayHi = require("./5-utils");
+console.log(sayHi);
+
+//noy we can access sayHi function too , but john & peter varibale is still not accessible as we exported them in an object
+//we can destructure it or use . operator
+const { john, peter } = names;
+
 sayHi("Susan");
 sayHi(john);
 sayHi(peter);
