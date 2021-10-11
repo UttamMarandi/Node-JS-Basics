644c19597944da2c3bfe72a7ae1c5010b26c12ed
Uttam Marandi <uttammarandi007@gmail.com>
2021-10-11 21:21:57 +0530

    app.put() , tesing in postman

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -26,6 +26,18 @@ app.post("/login", (req, res) => {
     res.status(401).send("Please provide credentials");
   }
 });
+//postman post test
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    //if user submit no name
+    res.status(400).json({ sucess: false, msg: "please provide name value" });
+  } else {
+    res.status(201).send({ sucess: true, data: [...people, name] }); //201 means sucessfully created
+    //append name to people array
+  }
+});
+
 //post for javascript login
 app.post("/api/people", (req, res) => {
   const { name } = req.body;
@@ -36,6 +48,27 @@ app.post("/api/people", (req, res) => {
     res.status(201).send({ sucess: true, person: name }); //201 means sucessfully created
   }
 });
+
+//app.put()
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params; //get the param id
+  const { name } = req.body; //get the name submitted by us as raw data in postman
+  console.log(id, name);
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    res.status(404).json({ sucess: false, msj: `no person with id ${id}` });
+  }
+  //change the name for the person id provided
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ sucess: true, data: newPeople });
+});
+
 app.listen(5000, (req, res) => {
   console.log("Server listening at port 5000");
 });
